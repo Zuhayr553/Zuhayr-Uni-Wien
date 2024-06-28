d8f6330588b5f7df0267461733dba9e455413f36
Zuhayr553 <zuhayrnoor444@gmail.com>
2024-06-28 20:12:32 +0200

    Initial

--- lib/configs/theme/app_styles.dart
+++ lib/configs/theme/app_styles.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:uni_wien_zuhayr_test/configs/colors/app_colors.dart';
 import 'package:uni_wien_zuhayr_test/configs/colors/colors_schemes.dart';
 import 'package:uni_wien_zuhayr_test/configs/theme/theme.dart';
 
